5775d22da5d510aa06a1de56bb746dd4ade03630
Marco Polichetti <gremo1982@gmail.com>
2018-02-17 01:31:38 +0100

    Use XML for services and remove parameters

--- src/DependencyInjection/HampeZurbInkExtension.php
+++ src/DependencyInjection/HampeZurbInkExtension.php
@@ -4,7 +4,7 @@ namespace Hampe\Bundle\ZurbInkBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class HampeZurbInkExtension extends Extension
@@ -17,7 +17,7 @@ class HampeZurbInkExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
